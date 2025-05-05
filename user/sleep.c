2d94252db63bdd10a7219294fa35b5d0949e9809
Leshan Chen <2400013135@stu.pku.edu.cn>
2025-05-05 21:48:43 +0800

    Complete UNIX program sleep

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int cnt;
+  if(argc != 2){
+    fprintf(2, "Usage: sleep [integer]\n");
+    exit(1);
+  }
+  cnt = atoi(argv[1]);
+  sleep(cnt);
+  exit(0);
+}
